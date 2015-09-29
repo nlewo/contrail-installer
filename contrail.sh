4309a28a592183d5801f72a9eea5c00095729b91
Antoine Eiche <antoine.eiche@cloudwatt.com>
2015-09-29 22:59:47 +0200

    Install libipfix-dev dependency

--- contrail.sh
+++ contrail.sh
@@ -319,7 +319,7 @@ function download_dependencies {
             apt_get install python-lxml python-redis python-jsonpickle
             apt_get install ant debhelper 
             apt_get install linux-headers-$(uname -r)
-            apt_get install libipfix
+            apt_get install libipfix libipfix-dev
             apt_get install python-docker-py
         fi	
         apt_get install python-neutron
